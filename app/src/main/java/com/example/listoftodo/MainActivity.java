f577507d8fd189774b1dc6a9c2516d6cf72db333
Raf13 <ra79605n@pace.edu>
2021-03-24 12:51:22 -0400

    Initial commit

---
+++ app/src/main/java/com/example/listoftodo/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.listoftodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView=(ListView)findViewById(R.id.listview);
+
+        ArrayList<String> arrayList=new ArrayList<>();
+
+        arrayList.add("android");
+        arrayList.add("is");
+        arrayList.add("great");
+        arrayList.add("android");
+        arrayList.add("is");
+        arrayList.add("great");
+        arrayList.add("android");
+        arrayList.add("is");
+        arrayList.add("great");
+        arrayList.add("android");
+        arrayList.add("is");
+        arrayList.add("great");
+        arrayList.add("android");
+        arrayList.add("is");
+        arrayList.add("great");
+
+        ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(arrayAdapter);
+
+    }
+}
